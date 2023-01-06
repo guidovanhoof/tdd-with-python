cc8e2bef6257b614de48e87b3c8c8484e5987c89
Guido Van Hoof <guido.van.hoof3@telenet.be>
2023-01-06 18:44:30 +0100

    renamed function

--- functional_tests.py
+++ functional_tests.py
@@ -10,16 +10,16 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self) -> None:
         self.visit_homepage()
         self.check_page_title_contains('To-Do')
 
         self.fail('Finish the test!')
 
-    def visit_homepage(self):
+    def visit_homepage(self) -> None:
         self.browser.get('http://localhost:8000')
 
-    def check_page_title_contains(self, search_for):
+    def check_page_title_contains(self, search_for) -> None:
         self.assertIn(search_for, self.browser.title)
 
     # Enter a new to-do item
